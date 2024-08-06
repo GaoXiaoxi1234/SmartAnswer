2b57a697cc558ea0a6784d09a4101134eb782af1
dshgz <1290325818@qq.com>
2024-08-06 10:44:03 +0800

    项目初始化

---
+++ smartanswer-backend/src/main/java/com/gxx/smartanswer/exception/BusinessException.java
@@ -0,0 +1,36 @@
+package com.gxx.smartanswer.exception;
+
+import com.gxx.smartanswer.common.ErrorCode;
+
+/**
+ * 自定义异常类
+ *
+ * @author <a href="https://github.com/GaoXiaoxi1234">小高同学</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public class BusinessException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
